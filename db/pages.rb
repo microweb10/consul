4ea9f5634596446488a2ef0314a9cd05bc212862
Julian Herrero <microweb10@gmail.com>
2019-03-04 15:25:33 +0100

    tests

--- db/pages.rb
+++ db/pages.rb
@@ -1,7 +1,21 @@
+@logger = Logger.new(STDOUT)
+
+def log(msg)
+  @logger.info "#{msg}\n"
+end
+
+log "pages/welcome_level_three_verified"
 require_relative "pages/welcome_level_three_verified"
+log "pages/welcome_level_two_verified"
 require_relative "pages/welcome_level_two_verified"
+log "pages/welcome_not_verified"
 require_relative "pages/welcome_not_verified"
+log "pages/accessibility"
 require_relative "pages/accessibility"
+log "pages/conditions"
 require_relative "pages/conditions"
+log "pages/privacy"
 require_relative "pages/privacy"
+log "pages/faq"
 require_relative "pages/faq"
+log "Done!"
